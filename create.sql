5759ed20f2b214e391a2ecb0e6380feb07ca2884
Muhammed Jasir <jasirmk01@gmail.com>
2021-07-17 13:09:48 +0530

    added create.sql file

---
+++ create.sql
@@ -0,0 +1,24 @@
+create table publisher (
+             id serial primary key,
+             name text,
+             books text,
+             country text
+                        );
+
+create table books    (
+             id serial primary key,
+             title text,
+             publisher serial references publisher(id)
+                        );
+                        
+create table subjects  (
+             id serial primary key ,
+             name text
+                       );
+                       
+create table books_subjects (
+             book serial references books(id),
+             subject serial references subjects(id)
+                             );
+                                   
+                                   
