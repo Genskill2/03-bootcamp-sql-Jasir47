8daa3312a62e042a5091edcfc0d67d661012909a
Muhammed Jasir <jasirmk01@gmail.com>
2021-07-17 13:10:34 +0530

    added insert.sql file

---
+++ insert.sql
@@ -0,0 +1,73 @@
+insert into books (title ,publisher) values ('The C Programming Language',1 );
+insert into books (title ,publisher) values ('The Go Programming Language',2);
+insert into books (title ,publisher) values ('The UNIX Programming Environment',3);
+insert into books (title ,publisher) values ('Cryptonomicon',4);
+insert into books (title ,publisher) values ('Deep Work',5);
+insert into books (title ,publisher) values ('Atomic Habits',6);
+insert into books (title ,publisher) values ('The City and The City',7);
+insert into books (title ,publisher) values ('The Great War for Civilisation',8);
+
+
+
+insert into publisher (name , books, country) values ('PHI','The C Programming Language','India');
+insert into publisher (name , books,country) values ('PHI','The Go Programming Language','India');
+insert into publisher (name , books,country) values ('PHI','The UNIX Programming Environment','India');
+insert into publisher (name , books,country) values ('Harper','Cryptonomicon','USA');
+insert into publisher (name , books,country) values ('GCP','Deep Work','USA');
+insert into publisher (name , books,country) values ('Avery','Atomic Habits','USA');
+insert into publisher (name , books, country) values ('Del Rey','The City and The City','UK');
+insert into publisher (name , books, country) values ('Vintage','The Great War for Civilisation','UK');
+
+
+
+insert into subjects (name ) values ('C');
+insert into subjects (name ) values ('UNIX');
+insert into subjects (name ) values ('Technology');
+insert into subjects (name ) values ('Go');
+insert into subjects (name ) values ('Science Fiction');
+insert into subjects (name ) values ('Productivity');
+insert into subjects (name ) values ('Psychology');
+insert into subjects (name ) values ('Politics');
+insert into subjects (name ) values ('History');
+
+
+
+insert into books_subjects (book , subject ) values (1,1);
+insert into books_subjects (book , subject ) values (1,2);
+insert into books_subjects (book , subject ) values (1,3);
+insert into books_subjects (book , subject ) values (2,4);
+insert into books_subjects (book , subject ) values (2,3);
+insert into books_subjects (book , subject ) values (3,2);
+insert into books_subjects (book , subject ) values (3,3);
+insert into books_subjects (book , subject ) values (4,3);
+insert into books_subjects (book , subject ) values (4,5);
+insert into books_subjects (book , subject ) values (5,3);
+insert into books_subjects (book , subject ) values (5,6);
+insert into books_subjects (book , subject ) values (6,6);
+insert into books_subjects (book , subject ) values (6,7);
+insert into books_subjects (book , subject ) values (7,5);
+insert into books_subjects (book , subject ) values (7,8);
+insert into books_subjects (book , subject ) values (8,8);
+insert into books_subjects (book , subject ) values (8,9);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
